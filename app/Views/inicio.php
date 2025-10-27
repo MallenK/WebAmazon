15ccc3d92ee2a881dfab8e62c597eae44d64804c
MallenK18 <sergimallenweb@gmail.com>
2025-10-27 21:11:43 +0100

    First Commit

---
+++ app/Views/inicio.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Mi proyecto</title>
+  <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+  <h1>Bienvenido a mi sitio CodeIgniter</h1>
+</body>
+</html>
